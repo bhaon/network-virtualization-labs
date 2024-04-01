0d4adff55270b76b5de9d096b531bc74a55bd853
bhaon <bhaon13@gmail.com>
2024-04-01 07:27:33 +0000

    presque

--- mpls-l3vpn/PC-interfaces.sh
+++ mpls-l3vpn/PC-interfaces.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
-sudo docker exec clab-frrlab-PC1 ip link set eth1 up
-sudo docker exec clab-frrlab-PC1 ip addr add 192.168.1.1/24 dev eth1
-sudo docker exec clab-frrlab-PC1 ip route add 0.0.0.0/0 via 192.168.1.254 dev eth1
+sudo docker exec clab-ldp-pc1 ip link set eth1 up
+sudo docker exec clab-ldp-pc1 ip addr add 192.168.1.1/24 dev eth1
+sudo docker exec clab-ldp-pc1 ip route add 0.0.0.0/0 via 192.168.1.254 dev eth1
 
-sudo docker exec clab-frrlab-PC2 ip link set eth1 up
-sudo docker exec clab-frrlab-PC2 ip addr add 192.168.2.1/24 dev eth1
-sudo docker exec clab-frrlab-PC2 ip route add 0.0.0.0/0 via 192.168.2.254 dev eth1
+sudo docker exec clab-ldp-pc2 ip link set eth1 up
+sudo docker exec clab-ldp-pc2 ip addr add 192.168.2.1/24 dev eth1
+sudo docker exec clab-ldp-pc2 ip route add 0.0.0.0/0 via 192.168.2.254 dev eth1
 
-sudo docker exec clab-frrlab-PC3 ip link set eth1 up
-sudo docker exec clab-frrlab-PC3 ip addr add 192.168.3.1/24 dev eth1
-sudo docker exec clab-frrlab-PC3 ip route add 0.0.0.0/0 via 192.168.3.254 dev eth1
+sudo docker exec clab-ldp-pc3 ip link set eth1 up
+sudo docker exec clab-ldp-pc3 ip addr add 192.168.3.1/24 dev eth1
+sudo docker exec clab-ldp-pc3 ip route add 0.0.0.0/0 via 192.168.3.254 dev eth1
 
-sudo docker exec clab-frrlab-PC4 ip link set eth1 up
-sudo docker exec clab-frrlab-PC4 ip addr add 192.168.4.1/24 dev eth1
-sudo docker exec clab-frrlab-PC4 ip route add 0.0.0.0/0 via 192.168.4.254 dev eth1
+sudo docker exec clab-ldp-pc4 ip link set eth1 up
+sudo docker exec clab-ldp-pc4 ip addr add 192.168.4.1/24 dev eth1
+sudo docker exec clab-ldp-pc4 ip route add 0.0.0.0/0 via 192.168.4.254 dev eth1
--- mpls-l3vpn/mtu.sh
+++ mpls-l3vpn/mtu.sh
@@ -1,29 +1,29 @@
 #!/bin/sh
-sudo docker exec clab-frrlab-p1 ifconfig eth1 mtu 1500
-sudo docker exec clab-frrlab-p1 ifconfig eth2 mtu 1500
-sudo docker exec clab-frrlab-p1 ifconfig eth3 mtu 1500
+sudo docker exec clab-ldp-p1 ifconfig eth1 mtu 1500
+sudo docker exec clab-ldp-p1 ifconfig eth2 mtu 1500
+sudo docker exec clab-ldp-p1 ifconfig eth3 mtu 1500
 
-sudo docker exec clab-frrlab-p2 ifconfig eth1 mtu 1500
-sudo docker exec clab-frrlab-p2 ifconfig eth2 mtu 1500
-sudo docker exec clab-frrlab-p2 ifconfig eth3 mtu 1500
-sudo docker exec clab-frrlab-p2 ifconfig eth4 mtu 1500
+sudo docker exec clab-ldp-p2 ifconfig eth1 mtu 1500
+sudo docker exec clab-ldp-p2 ifconfig eth2 mtu 1500
+sudo docker exec clab-ldp-p2 ifconfig eth3 mtu 1500
+sudo docker exec clab-ldp-p2 ifconfig eth4 mtu 1500
 
-sudo docker exec clab-frrlab-p3 ifconfig eth1 mtu 1500
-sudo docker exec clab-frrlab-p3 ifconfig eth2 mtu 1500
+sudo docker exec clab-ldp-p3 ifconfig eth1 mtu 1500
+sudo docker exec clab-ldp-p3 ifconfig eth2 mtu 1500
 
-sudo docker exec clab-frrlab-p4 ifconfig eth1 mtu 1500
-sudo docker exec clab-frrlab-p4 ifconfig eth2 mtu 1500
-sudo docker exec clab-frrlab-p4 ifconfig eth3 mtu 1500
+sudo docker exec clab-ldp-p4 ifconfig eth1 mtu 1500
+sudo docker exec clab-ldp-p4 ifconfig eth2 mtu 1500
+sudo docker exec clab-ldp-p4 ifconfig eth3 mtu 1500
 
-sudo docker exec clab-frrlab-p5 ifconfig eth1 mtu 1500
-sudo docker exec clab-frrlab-p5 ifconfig eth2 mtu 1500
-sudo docker exec clab-frrlab-p5 ifconfig eth3 mtu 1500
-sudo docker exec clab-frrlab-p5 ifconfig eth4 mtu 1500
+sudo docker exec clab-ldp-p5 ifconfig eth1 mtu 1500
+sudo docker exec clab-ldp-p5 ifconfig eth2 mtu 1500
+sudo docker exec clab-ldp-p5 ifconfig eth3 mtu 1500
+sudo docker exec clab-ldp-p5 ifconfig eth4 mtu 1500
 
-sudo docker exec clab-frrlab-ce1a ifconfig eth1 mtu 1500
+sudo docker exec clab-ldp-ce1a ifconfig eth1 mtu 1500
 
-sudo docker exec clab-frrlab-ce1b ifconfig eth1 mtu 1500
+sudo docker exec clab-ldp-ce1b ifconfig eth1 mtu 1500
 
-sudo docker exec clab-frrlab-ce2a ifconfig eth1 mtu 1500
+sudo docker exec clab-ldp-ce2a ifconfig eth1 mtu 1500
 
-sudo docker exec clab-frrlab-ce2b ifconfig eth1 mtu 1500
+sudo docker exec clab-ldp-ce2b ifconfig eth1 mtu 1500
